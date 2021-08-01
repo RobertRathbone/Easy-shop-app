7cf33d5d5acff25f01dccdf8897637b64ce996aa
RobertRathbone <BobRathbone@gmail.com>
2021-08-01 08:26:28 -0700

    Create user

--- models/product.js
+++ models/product.js
@@ -29,11 +29,11 @@ const productSchema = mongoose.Schema({
         type: Number,
         default: 0
     },
-    // category: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'Category',
-    //     required: true
-    // },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
     countInStock: {
         type: Number,
         required: true,
@@ -55,5 +55,13 @@ const productSchema = mongoose.Schema({
     dateCreated: { type: Date, default: Date.now }
 
 })
+
+productSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+productSchema.set('toJSON', {
+    virtuals : true,
+});
 
 exports.Product = mongoose.model('Product', productSchema);
