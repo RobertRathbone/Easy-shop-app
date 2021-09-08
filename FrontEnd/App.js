b72779333e6c6fe94373b037dac338cef3b2b1e9
RobertRathbone <BobRathbone@gmail.com>
2021-09-07 23:49:16 -0700

    Wrap in Provider

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -3,8 +3,12 @@ import React from 'react';
 import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './Redux/store';
+
 // Navigators
-import Main from '.Navigators/Main';
+import Main from './Navigators/Main';
 
 // Screens
 import Header from './Shared/Header';
@@ -15,10 +19,12 @@ LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Header />
-      <Main />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Header />
+        <Main />
+      </NavigationContainer>
+    </Provider>
   );
 }
 
